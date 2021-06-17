d1e32c9008336a6d820fd0d99a07776b893021a1
robot-ux <robot.ux@protonmail.com>
2021-06-17 14:26:04 +0800

    fix: Support new ledger live

--- src/index.ts
+++ src/index.ts
@@ -1,181 +1,190 @@
-import { ledger as Ledger, crypto } from '@binance-chain/javascript-sdk';
-import LedgerApp, {
-  Version,
-} from '@binance-chain/javascript-sdk/lib/ledger/ledger-app';
+import { crypto, Transaction } from '@binance-chain/javascript-sdk';
+import EventEmitter from 'events';
 
-import {
-  DEFAULT_LEDGER_INTERACTIVE_TIMEOUT,
-  DEFAULT_LEDGER_NONINTERACTIVE_TIMEOUT,
-  DEFAULT_LEDGER_TRANSPORT,
-  DEFAULT_LEDGER_OPEN_TIMEOUT,
-  LEDGER_EXPECTED_MAJOR_VERSION,
-  DEFAULT_GET_ADDRESSES_LIMIT,
-  DEFAULT_HD_PATH,
-} from './constants';
-import { TransportKeys, Props } from './types';
+const DEFAULT_HD_PATH = [44, 714, 0, 0, 0];
+const BRIDGE_URL = 'https://robot-ux.github.io/bc-ledger-bridge';
 
-export class LedgerClient {
-  private _transportKey: TransportKeys;
-  private _interactiveTimeout: number;
-  private _nonInteractiveTimeout: number;
-  private _app: LedgerApp | null;
-  private _version: Version;
-  private _hdPath: Array<number>;
-  public page: number;
+class BcLedgerBridge extends EventEmitter {
+  hdPath: Array<number>;
+  page: number;
+  perPage: number;
+  iframe: any;
+  bridgeUrl: string;
+  hrp: string;
+  iframeLoaded: boolean;
+  delayedPromise: any;
 
-  constructor({
-    transportKey = DEFAULT_LEDGER_TRANSPORT,
-    interactiveTimeout = DEFAULT_LEDGER_INTERACTIVE_TIMEOUT,
-    nonInteractiveTimeout = DEFAULT_LEDGER_NONINTERACTIVE_TIMEOUT,
-  }: Props) {
-    this._transportKey = transportKey;
-    this._interactiveTimeout = interactiveTimeout;
-    this._nonInteractiveTimeout = nonInteractiveTimeout;
-    this._app = null;
-    this._version = {} as Version;
-    this._hdPath = DEFAULT_HD_PATH;
+  constructor() {
+    super();
     this.page = 0;
+    this.perPage = 5;
+    this.iframe = null;
+    this.hdPath = DEFAULT_HD_PATH;
+    this.bridgeUrl = BRIDGE_URL;
+    this.hrp = 'bnb';
+    this.iframeLoaded = false;
+    this.delayedPromise = {};
+
+    this._setupIframe();
   }
 
-  async unlock() {
-    if (!this._app) {
-      let transImpl = Ledger.transports[this._transportKey];
-      if (this._transportKey === 'node' && !transImpl) {
-        transImpl = require('@ledgerhq/hw-transport-node-hid').default;
+  setHdPath(hdPath: Array<number>) {
+    this.hdPath = hdPath;
+  };
+
+  setHrp(hrp: string) {
+    this.hrp = hrp;
+  };
+
+  unlock(): Promise<Array<string>> {
+    return new Promise( (resolve, reject) => {
+      this._sendMessage({
+        action: 'ledger-unlock',
+        params: {
+          hdPath: this.hdPath.toString(),
+          hrp: this.hrp
+        }
+      }, function ({ success, payload }: { success: boolean, payload: any }) {
+        if (success) {
+          resolve(payload);
+        } else {
+          reject(payload.error || 'Unknown error');
+        }
+      });
+    });
+  };
+
+  signTransaction(tx: Transaction, hdPath: Array<number>): Promise<Transaction> {
+    return new Promise((resolve, reject) => {
+      const signBytes = tx.getSignBytes();
+
+      this._sendMessage({
+        action: 'ledger-sign-transaction',
+        params: {
+          tx: signBytes.toString('hex'),
+          hdPath: hdPath.toString(),
+          hrp: this.hrp
+        }
+      }, ({ success, payload }: any) => {
+        if (success) {
+          var pubKey = crypto.getPublicKey(payload.pubkey);
+          tx.addSignature(pubKey, Buffer.from(payload.signature, 'hex'));
+          resolve(tx);
+        } else {
+          reject(new Error(payload.error || 'Ledger: Unknown error while signing transaction'));
+        }
+      });
+    });
+  };
+
+  updateTransportMethod (useLedgerLive = false) {
+    return new Promise((resolve, reject) => {
+      // If the iframe isn't loaded yet, let's store the desired useLedgerLive value and
+      // optimistically return a successful promise
+      if (!this.iframeLoaded) {
+        this.delayedPromise = {
+          resolve,
+          reject,
+          useLedgerLive,
+        }
+        return
       }
 
-      const transInst = await transImpl.create(DEFAULT_LEDGER_OPEN_TIMEOUT);
+      this._sendMessage({
+        action: 'ledger-update-transport',
+        params: { useLedgerLive },
+      }, ({ success }: { success: boolean }) => {
+        if (success) {
+          resolve(true)
+        } else {
+          reject(new Error('Ledger transport could not be updated'))
+        }
+      })
+    })
+  }
 
-      // eslint-disable-next-line
-      const app = new Ledger.app(
-        transInst,
-        this._interactiveTimeout,
-        this._nonInteractiveTimeout
-      );
+  getFirstPage() {
+    this.page = 0;
+    return this.__getPage(1);
+  };
 
-      const version = (this._version = await app.getVersion());
-      if (version.device_locked) {
-        throw new Error('Ledger Device is locked, please unlock it first');
+  getNextPage() {
+    return this.__getPage(1);
+  };
+
+  getPreviousPage() {
+    return this.__getPage(-1);
+  };
+
+  async __getPage (increment: number) {
+
+    this.page += increment
+
+    if (this.page <= 0) {
+      this.page = 1
+    }
+    const from = (this.page - 1) * this.perPage
+    this.hdPath.splice(this.hdPath.length - 1, 1, from);
+
+    const accounts = await this.unlock() as []
+    return accounts.map((address, index) => {
+      return {
+        address: address,
+        index: index,
+        balance: null
+      };
+    })
+  }
+
+  _setupIframe() {
+    this.iframe = document.createElement('iframe');
+    this.iframe.src = this.bridgeUrl;
+    this.iframe.contentWindow.onload = async () => {
+      // If the ledger live preference was set before the iframe is loaded,
+      // set it after the iframe has loaded
+      this.iframeLoaded = true
+      if (this.delayedPromise) {
+        try {
+          const result = await this.updateTransportMethod(
+            this.delayedPromise.useLedgerLive,
+          )
+          this.delayedPromise.resolve(result)
+        } catch (e) {
+          this.delayedPromise.reject(e)
+        } finally {
+          delete this.delayedPromise
+        }
+      }
+    }
+    document.head.appendChild(this.iframe);
+  };
+
+  _getOrigin() {
+    var tmp = this.bridgeUrl.split('/');
+    tmp.splice(-1, 1);
+    return tmp.join('/');
+  };
+
+  _sendMessage(msg: any, cb: any) {
+    msg.target = 'BC-LEDGER-IFRAME';
+    this.iframe.contentWindow.postMessage(msg, '*');
+
+    const eventListener = ({ origin, data }: any) => {
+      if (origin !== this._getOrigin()) {
+        return false;
       }
 
-      if (version.major !== LEDGER_EXPECTED_MAJOR_VERSION) {
-        throw new Error(
-          `Ledger: Expected app major version ${LEDGER_EXPECTED_MAJOR_VERSION}, but got ${version.major}`
-        );
+      if (data && data.action && data.action === msg.action + "-reply") {
+        cb(data);
+        return undefined;
       }
 
-      this._app = app;
-      this._version = version;
-    }
-  }
+      window.removeEventListener('message', eventListener);
+      return undefined;
+    };
 
-  async getFirstPage() {
-    return await this.getAddresses({
-      hdPathStart: DEFAULT_HD_PATH,
-    });
-  }
-
-  async getPreviousPage() {
-    const index = (this.page - 1) * DEFAULT_GET_ADDRESSES_LIMIT;
-    const hdPath = this._hdPath.splice(this._hdPath.length - 1, 1, index);
-    if (this.page > 0) {
-      this.page -= 1;
-    }
-    return await this.getAddresses({
-      hdPathStart: hdPath,
-    });
-  }
-
-  async getNextPage() {
-    const index = (this.page + 1) * DEFAULT_GET_ADDRESSES_LIMIT;
-    const hdPath = this._hdPath.splice(this._hdPath.length - 1, 1, index);
-    this.page += 1;
-    return await this.getAddresses({
-      hdPathStart: hdPath,
-    });
-  }
-
-  setHdPath(hdPath: Array<number> = DEFAULT_HD_PATH) {
-    this._hdPath = hdPath;
-  }
-
-  mustHaveApp() {
-    if (this._app === null) {
-      throw new Error('LedgerClient: Call init() first');
-    }
-    return this._app;
-  }
-
-  getApp() {
-    return this._app;
-  }
-
-  async getVersion() {
-    this.mustHaveApp();
-    if (this._version) {
-      return Promise.resolve(this._version);
-    }
-    return this._app?.getVersion();
-  }
-
-  async getPublicKey(hdPath: number[]) {
-    return this.mustHaveApp().getPublicKey(hdPath);
-  }
-
-  async getAddress(hdPath: number[]) {
-    const response = await this.getPublicKey(hdPath);
-    const { pk } = response;
-    const address = crypto.getAddressFromPublicKey(
-      (pk as Buffer).toString('hex')
-    );
-    return address;
-  }
-
-  async getAddresses({
-    hdPathStart,
-    hrp = 'bnb',
-    limit = DEFAULT_GET_ADDRESSES_LIMIT,
-  }: {
-    hdPathStart: Array<number>;
-    hrp?: string;
-    limit?: number;
-  }) {
-    if (!Array.isArray(hdPathStart) || hdPathStart.length < 5) {
-      throw new Error(
-        'hdPathStart must be an array containing at least 5 path nodes'
-      );
-    }
-    const addresses = [];
-    let hdPath = hdPathStart;
-    for (let i = 0; i < limit; i++) {
-      if (Number(hdPath[hdPath.length - 1]) > 0x7fffffff) break;
-
-      const response = await this.getPublicKey(hdPath);
-      const { pk } = response;
-      const address = crypto.getAddressFromPublicKey(
-        (pk as Buffer).toString('hex'),
-        hrp
-      );
-      addresses.push(address);
-
-      hdPath = hdPathStart.slice();
-      hdPath[hdPath.length - 1] = i + hdPath[hdPath.length - 1] + 1;
-    }
-    return addresses;
-  }
-
-  async showAddress(hrp = 'bnb', hdPath: number[] = [44, 714, 0, 0, 0]) {
-    if (
-      this._version &&
-      this._version.major === 1 &&
-      this._version.minor === 0
-    ) {
-      throw new Error('Ledger: showAddress is not supported in app v1.0.x!');
-    }
-    return this.mustHaveApp().showAddress(hrp, hdPath);
-  }
-
-  async sign(signBytes: Buffer, hdPath: number[]) {
-    return this.mustHaveApp().sign(signBytes, hdPath);
-  }
+    window.addEventListener('message', eventListener);
+  };
 }
+
+export { BcLedgerBridge };
