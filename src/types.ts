a59eda59c3f5f1c90bce7d90418caaf7026f3115
robot-ux <robot.ux@protonmail.com>
2021-06-22 21:40:59 +0800

    fix: use difference transport method for chrome and firefox

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,7 @@ export const TRANSPORT_KEYS = ['u2f', 'node', 'wble'] as const;
 
 export type TransportKeys = typeof TRANSPORT_KEYS[number];
 
+
 export interface Props {
   transportKey: TransportKeys;
   interactiveTimeout?: number;
